d27d982d7f928581f403772d1cf8974abc0b58b6
vedant241206 <vedantpawar434@gmail.com>
2025-10-04 15:57:51 +0530

    chore: refresh timestamp

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
-
+//ds
 export default tseslint.config(
   { ignores: ["dist"] },
   {
